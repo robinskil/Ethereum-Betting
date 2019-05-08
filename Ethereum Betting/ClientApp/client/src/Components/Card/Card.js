5683a8c49e48cde73e3b2c9288f09dccd5feae56
oscar <druiventakoscar@gmail.com>
2019-05-09 00:57:13 +0200

    Add Card component

---
+++ Ethereum Betting/ClientApp/client/src/Components/Card/Card.js
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import "./style.css";
+
+export class Card extends React.Component {
+    render() {
+        const { title, subtitle, children } = this.props;
+
+        return <div className="custom-card card">
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                {
+                    subtitle && (<h6 className="card-subtitle mb-2 text-muted">subtitle</h6>)
+                }
+                {children}
+            </div>
+        </div>
+    }
+}
