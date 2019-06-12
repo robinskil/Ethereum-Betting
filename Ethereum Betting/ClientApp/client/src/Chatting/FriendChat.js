f8367d6d93c3c671484c79ab638b3037c4a5fde7
StevenW1999 <0932549@hr.nl>
2019-06-12 14:54:43 +0200

    friendslist styling

--- Ethereum Betting/ClientApp/client/src/Chatting/FriendChat.js
+++ Ethereum Betting/ClientApp/client/src/Chatting/FriendChat.js
@@ -34,7 +34,7 @@ export default class FriendsChat extends React.Component {
         return (
             <div> TEst
 
-                      <button class="open-button" type="button" onClick={this.Toggle}>Friends</button>
+                      <button class="btn btn-primary open-button" type="button" onClick={this.Toggle}>Friends</button>
                       {this.state.Toggled ? <FriendsList />: null}
                 
      
@@ -203,7 +203,7 @@ class SearchBar extends React.Component {
 
     render() {
         return (
-            <div className = "col-md-12">
+            <div>
             <form>
                 <input
                     className="form-control"
@@ -241,9 +241,10 @@ class NewContactRow extends React.Component {
         return (
 
             <form className="form-inline" onSubmit={this.handleSubmit}>
-                <div className="form-group row">
-                    <div className="col-md-3">
+                <div>
+                    <div >
                         <input type="text" name="name" className="form-control" id="nameInput" placeholder="add friend" />
+                        <button type="submit" className="btn btn-primary"><i className="fa fa-fw fa-plus"></i>Add</button>
                     </div>
                
            
