f5805e0209c1d546b34bd78c00b13ec815acb69f
amfigaroa <amfigaroa@gmail.com>
2019-06-13 02:16:05 +0200

    Touch my tralalala
    SlidingPuzzle + ieks in FriendRepo

--- InteractorLayer/SlidingPuzzleBet/SlidingPuzzle.cs
+++ InteractorLayer/SlidingPuzzleBet/SlidingPuzzle.cs
@@ -1,33 +1,102 @@
 ﻿using InteractorLayer.RequestModels;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
 namespace InteractorLayer.SlidingPuzzleBet
 {
+
     public class SlidingPuzzle : IDisposable
     {
         public int[,] Puzzle { get; }
+        public int[,] Answer { get; }
+        public SlidingPuzzleMoveModel Black { get; set; }
+
+        private Random Random { get; }
+
+        public List<int> Numbers { get; }
         public Stopwatch Time { get; }
         public double CompletedTime { get; }
         public SlidingPuzzle()
+
+
         {
-            Puzzle = new int[4,4];
+            Random = new Random();
+            Puzzle = new int[4, 4];
+            Black = new SlidingPuzzleMoveModel();
+            Answer = new int[Puzzle.GetLength(0), Puzzle.GetLength(1)];
+            Numbers = new List<int>();
             GenerateRandomPuzzle();
+            
             Time = new Stopwatch();
             Time.Start();
         }
 
+
+        public int FindDifference(int nr1, int nr2)
+        {
+            int a = Math.Abs(nr1 - nr2);
+            return a;
+        }
+
+        
         public bool DoMove(SlidingPuzzleMoveModel moveModel)
         {
-            throw new NotImplementedException();
+            if ((FindDifference(Black.X, moveModel.X) == 1) && (FindDifference(Black.Y, moveModel.Y) == 0) || 
+                (FindDifference(Black.Y, moveModel.Y) == 1) && (FindDifference(Black.X, moveModel.X) == 0))
+            {
+
+                int old_black = Puzzle[Black.X, Black.Y];
+                int old_moveModel = Puzzle[moveModel.X, moveModel.Y];
+
+                Puzzle[Black.X, Black.Y] = old_moveModel;
+                Puzzle[moveModel.X, moveModel.Y] = old_black;
+
+
+
+                return true;
+
+            }else{
+
+                return false;
+            }
+            //throw new NotImplementedException();
         }
 
         public bool TryFinish()
         {
-            Time.Stop();
-            throw new NotImplementedException();
+            var equal =
+                Puzzle.Rank == Answer.Rank &&
+                Enumerable.Range(0, Puzzle.Rank).All(dimension => Puzzle.GetLength(dimension) == Answer.GetLength(dimension)) &&
+                Puzzle.Cast<int>().SequenceEqual(Answer.Cast<int>());
+
+            if (equal == true)
+            {
+                Time.Stop();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+            //throw new NotImplementedException();
+        }
+
+        public void Shuffle(List<int> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = this.Random.Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
 
         /// <summary>
@@ -35,14 +104,40 @@ namespace InteractorLayer.SlidingPuzzleBet
         /// </summary>
         private void GenerateRandomPuzzle()
         {
-            for (int i = 0; i < Puzzle.GetLength(0); i++)
+
+            for (int a = 0; a < Puzzle.Length; a++)
             {
-                for (int j = 0; j < Puzzle.GetLength(1); j++)
+                Numbers.Add(a);
+            }
+            Shuffle(Numbers);
+
+            int cnt = 0;
+            int cnt2 = 1;
+
+            for (int y = 0; y < Puzzle.GetLength(1); y++)
+            {
+
+                for (int x = 0; x < Puzzle.GetLength(0); x++)
                 {
 
+                    Puzzle[x,y] = Numbers[cnt];
+
+                    if (Numbers[cnt] == 0)
+                    {
+                        Black.X = x;
+                        Black.Y = y;
+                    }
+                    Answer[x,y] = cnt2;
+
+                    cnt++;
+                    cnt2++;
+                    
                 }
             }
-            throw new NotImplementedException();
+            Answer[(Puzzle.GetLength(0)-1), (Puzzle.GetLength(1)-1)] = 0;
+
+
+            //throw new NotImplementedException();
         }
 
         #region IDisposable Support
@@ -80,4 +175,6 @@ namespace InteractorLayer.SlidingPuzzleBet
         }
         #endregion
     }
+
 }
+
