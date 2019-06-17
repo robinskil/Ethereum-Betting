0643a60eb6bd52cf5e161649e78c1c2b5bb211aa
amfigaroa <amfigaroa@gmail.com>
2019-06-17 13:22:07 +0200

    UnittestGenerateRandomPuzzle
    Unit test added

--- Unit Testing Ethereum Betting/TestGenerateRandomPuzzle.cs
+++ Unit Testing Ethereum Betting/TestGenerateRandomPuzzle.cs
@@ -16,21 +16,44 @@ namespace Unit_Testing_Ethereum_Betting
         /// Test if it creates a Random generated puzzle.
         /// </summary>
         [Fact]
-        public void TestSlidingPuzzle()
+        public void TestGenerateSlidingPuzzle()
         {
             SlidingPuzzle result = new SlidingPuzzle();
-            Assert.NotNull(result);
+            Assert.False(result.TryFinish());
         }
 
         /// <summary>
-        /// Test if 
+        /// Test if it creates a 4X4 puzzle.
+        /// </summary>
+        [Fact]
+        public void TestPuzzleSize()
+        {
+            SlidingPuzzle s = new SlidingPuzzle();
+            int result = s.Puzzle.GetLength(0);
+            int result2 = s.Puzzle.GetLength(1);
+            int result3 = 0;
+            if (s.Puzzle.Rank > 1) {
+                for (int d = 0; d < s.Puzzle.Rank; d++)
+                {
+                    result3++;
+                }
+            }
+
+            Assert.True(result == 4 && result2 == 4 && result3 == 2);
+        }
+
+        /// <summary>
+        /// Test if DoMove() returns false when a wrong moveModel is inserted: X = Puzzle.GetLength(0) + 1, Y= Puzzle.GetLength(1) + 1
         /// </summary>
         [Fact]
         public void TestDoMove()
         {
             SlidingPuzzle s = new SlidingPuzzle();
-            SlidingPuzzleMoveModel moveModel = new SlidingPuzzleMoveModel() { X = 5, Y = 5 };
-            var result = s.DoMove(moveModel);
+            SlidingPuzzleMoveModel moveModel = new SlidingPuzzleMoveModel() { X = s.Puzzle.GetLength(0) + 1, Y = s.Puzzle.GetLength(1) + 1 };
+            bool result = s.DoMove(moveModel);
+            Assert.False(result);
         }
+
+
     }
 }
